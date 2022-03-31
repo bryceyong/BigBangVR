90aea70018e4226e7ba3a67b16de8e556415f624
picrossin <45524258+driedel152@users.noreply.github.com>
2022-03-31 17:02:54 -0500

    init

--- Assets/Scripts/WaterFlow.cs
+++ Assets/Scripts/WaterFlow.cs
@@ -15,7 +15,7 @@ public class WaterFlow : MonoBehaviour
     {
        
         float zNew = transform.position.z + direction * speed * Time.deltaTime;
-        transform.position = new Vector3(30, 1.5f, zNew);
+        transform.position = new Vector3(30, 0.5f, zNew);
         if(zNew < zMax ){
             direction = 0;
         }
