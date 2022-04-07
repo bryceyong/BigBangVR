fc839f96f2963a064eef8ece0d08daccfaa18280
Bryce Jen Wei Yong <54676043+bryceyong@users.noreply.github.com>
2022-04-07 16:06:46 -0500

    comp fix

--- Assets/Scripts/NarratorChap3.cs
+++ Assets/Scripts/NarratorChap3.cs
@@ -12,7 +12,6 @@ public class NarratorChap3 : MonoBehaviour
 		"Earth is composed of multiple layers.",
 		"The first layer, the crust, the thinnest of all layers. The crust is where all of civilization is built.",
 		"The mantle is the second outermost layer full of molten magma.",
-		"
 		"The outer layer is composed of iron and nickel in liquid form and the inner core is composed of condensed iron about  ¾ of the moon with an extremely hot surface.",
 	};
 
