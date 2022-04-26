7bd46734f9e8b0bd63c6ef60f172fcfd3bc29ac3
Bryce Jen Wei Yong <54676043+bryceyong@users.noreply.github.com>
2022-04-25 21:42:50 -0500

    stuffs

--- Assets/Scripts/WaterFlow.cs
+++ Assets/Scripts/WaterFlow.cs
@@ -6,7 +6,7 @@ public class WaterFlow : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 1.0f;
-    private float zMax = -45.0f;
+    private float zMax = -55.0f;
 
  //starting position
     public static int direction = 0; //positive to start
