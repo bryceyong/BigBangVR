a62706660887415a69856da4844eb024720ff8c5
Bryce Jen Wei Yong <54676043+bryceyong@users.noreply.github.com>
2022-05-03 11:48:42 -0500

    init

--- Assets/Scripts/spin.cs
+++ Assets/Scripts/spin.cs
@@ -13,6 +13,6 @@ public class spin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, -15, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, -15 * Time.deltaTime, 0) );
     }
 }
